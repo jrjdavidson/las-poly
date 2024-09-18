7144c1c63c5bd194bab21dcbe53459fe3867dd1c
Jonathan Davidson <jonathan.davidson@canterbury.ac.nz>
2024-09-19 08:04:51 +1200

    working function, included tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
-use geo::ConvexHull;
-use geo::{Coord, LineString, Polygon};
+use geo::{ConvexHull, Coord, LineString, Polygon};
 use las::Reader;
 use serde_json::Map;
+use std::collections::HashMap;
 use std::error::Error;
 use std::sync::mpsc;
 use std::thread;
@@ -53,31 +53,93 @@ pub fn process_folder(
 
     drop(feature_tx); // Close the channel to signal completion
 
-    // Collect features from the channel
     let mut features = Vec::new();
-    for feature in feature_rx {
-        if group_by_folder {
+
+    // Collect features from the channel
+    if group_by_folder {
+        let mut features_by_folder: HashMap<String, Vec<Geometry>> = HashMap::new();
+        for feature in feature_rx {
+            let folder_path = feature
+                .properties
+                .as_ref()
+                .unwrap()
+                .get("folder_path")
+                .unwrap()
+                .as_str()
+                .unwrap()
+                .to_string();
+            let geometry = feature.geometry.unwrap();
+
+            features_by_folder
+                .entry(folder_path)
+                .or_default()
+                .push(geometry);
+        }
+
+        // Merge geometries for each folder path
+        for (folder_path, geometries) in features_by_folder {
+            let merged_polygon = geometries.into_iter().fold(
+                Polygon::new(LineString::new(vec![]), vec![]),
+                |acc, geometry| {
+                    if let Value::Polygon(geom_coords) = geometry.value {
+                        let mut coords: Vec<Coord<f64>> = acc.exterior().clone().into_inner();
+                        let new_coords: Vec<Coord<f64>> = geom_coords[0]
+                            .iter()
+                            .map(|c| Coord { x: c[0], y: c[1] })
+                            .collect();
+                        coords.extend(new_coords);
+
+                        // Create a LineString from the combined coordinates
+                        let line_string = LineString::from(coords);
+
+                        // Compute the convex hull to get a single enclosing polygon
+                        line_string.convex_hull()
+                    } else {
+                        acc
+                    }
+                },
+            );
+
+            let exterior_coords: Vec<Vec<f64>> = merged_polygon
+                .exterior()
+                .coords()
+                .map(|c| vec![c.x, c.y])
+                .collect();
+            let geojson_polygon = Value::Polygon(vec![exterior_coords]);
+            let geometry = Geometry::new(geojson_polygon);
+            let mut properties = Map::new();
+            properties.insert("folder_path".to_string(), folder_path.into());
+
+            let feature = Feature {
+                geometry: Some(geometry),
+                properties: Some(properties),
+                id: None,
+                bbox: None,
+                foreign_members: None,
+            };
+
             features.push(feature);
-            features.groupby("folder_path");
-        } else {
+        }
+    } else {
+        for feature in feature_rx {
             features.push(feature);
         }
     }
-    // Group features by folder path
 
-    // Create a FeatureCollection from all the features
+    // Create a FeatureCollection from all the merged features
     let feature_collection = FeatureCollection {
-        features,
+        features: features,
         bbox: None,
         foreign_members: None,
     };
+
     let geojson = GeoJson::FeatureCollection(feature_collection);
 
     // Save the GeoJSON to a file
-    let mut file = File::create("polygons.geojson")?;
+    let mut file = File::create("las_outlines.geojson")?;
     file.write_all(geojson.to_string().as_bytes())?;
 
-    println!("Polygons saved to polygons.geojson");
+    println!("Merged polygons saved to las_outlines.geojson");
 
     Ok(())
 }
@@ -133,6 +195,25 @@ fn create_polygon(file_path: &str, use_simple_outline: bool) -> Result<Feature, 
         );
     }
 
+    // Add additional properties from the LAS header
+    let header = reader.header();
+    properties.insert(
+        "number_of_points".to_string(),
+        header.number_of_points().into(),
+    );
+    if let Some(date) = header.date() {
+        properties.insert("date".to_string(), date.to_string().into());
+    }
+    properties.insert("file_source_id".to_string(), header.file_source_id().into());
+    properties.insert(
+        "generating_software".to_string(),
+        header.generating_software().into(),
+    );
+    properties.insert(
+        "system_identifier".to_string(),
+        header.system_identifier().into(),
+    );
+
     let feature = Feature {
         geometry: Some(geometry),
         properties: Some(properties),
@@ -142,4 +223,185 @@ fn create_polygon(file_path: &str, use_simple_outline: bool) -> Result<Feature, 
     };
 
     Ok(feature)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::fs;
+    use std::path::Path;
+
+    #[test]
+    fn test_create_polygon_simple_outline() {
+        let file_path = "tests/data/input1.las";
+        let result = create_polygon(file_path, true);
+        assert!(result.is_ok());
+        let feature = result.unwrap();
+        println!("{:?}", feature);
+        assert!(feature.geometry.is_some());
+
+        // Additional assertions
+        let geometry = feature.geometry.unwrap();
+        if let geojson::Value::Polygon(polygon) = geometry.value {
+            assert_eq!(polygon.len(), 1); // Ensure there's one polygon
+            assert_eq!(polygon[0].len(), 5); // Ensure the polygon has 5 points (including the closing point)
+            assert_eq!(polygon[0][0], [1771068.3800000001, 5917200.0]); // Check the first point
+            assert_eq!(polygon[0][1], [1771359.999, 5917200.0]); // Check the second point
+            assert_eq!(polygon[0][2], [1771359.999, 5917354.289]); // Check the third point
+            assert_eq!(polygon[0][3], [1771068.3800000001, 5917354.289]); // Check the fourth point
+            assert_eq!(polygon[0][4], [1771068.3800000001, 5917200.0]); // Check the closing point
+        } else {
+            panic!("Expected a Polygon geometry");
+        }
+
+        // Check properties
+        let properties = feature.properties.unwrap();
+        assert_eq!(properties.get("filename").unwrap(), "tests/data/input1.las");
+        assert_eq!(properties.get("folder_path").unwrap(), "tests/data");
+        assert_eq!(properties.get("number_of_points").unwrap(), 97359);
+    }
+
+    #[test]
+    fn test_create_polygon_convex_hull() {
+        let file_path = "tests/data/input2.las";
+        let result = create_polygon(file_path, false);
+        assert!(result.is_ok());
+        let feature = result.unwrap();
+        println!("{:?}", feature);
+        assert!(feature.geometry.is_some());
+
+        // Additional assertions
+        let geometry = feature.geometry.unwrap();
+        if let geojson::Value::Polygon(polygon) = geometry.value {
+            assert_eq!(polygon.len(), 1); // Ensure there's one polygon
+            assert_eq!(polygon[0].len(), 30); // Ensure the polygon has 30 points (including the closing point)
+            assert_eq!(polygon[0][0], [1771360.006, 5917201.84]); // Check the first point
+            assert_eq!(polygon[0][1], [1771360.026, 5917200.476]); // Check the second point
+            assert_eq!(polygon[0][2], [1771360.064, 5917200.029]); // Check the third point
+            assert_eq!(polygon[0][3], [1771360.307, 5917200.009]); // Check the fourth point
+            assert_eq!(polygon[0][29], [1771360.006, 5917201.84]); // Check the closing point
+        } else {
+            panic!("Expected a Polygon geometry");
+        }
+
+        // Check properties
+        let properties = feature.properties.unwrap();
+        assert_eq!(properties.get("filename").unwrap(), "tests/data/input2.las");
+        assert_eq!(properties.get("folder_path").unwrap(), "tests/data");
+    }
+
+    #[test]
+    fn test_process_folder_no_group_by_folder() {
+        let folder_path = "tests/data";
+        let result = process_folder(folder_path, false, false);
+        println!("{:?}", result);
+        assert!(result.is_ok());
+
+        // Check if the output file is created
+        let output_path = Path::new("las_outlines.geojson");
+        assert!(output_path.exists());
+
+        // Read the file and perform checks
+        let geojson_str = fs::read_to_string(output_path).unwrap();
+        let geojson: GeoJson = geojson_str.parse().unwrap();
+        if let GeoJson::FeatureCollection(fc) = geojson {
+            println!("{:?}", fc);
+            assert_eq!(fc.features.len(), 2); // Ensure there are two features
+
+            // Check the first feature
+            let feature1 = &fc.features[0];
+            assert!(feature1.geometry.is_some());
+            let geometry1 = feature1.geometry.as_ref().unwrap();
+            if let geojson::Value::Polygon(polygon) = &geometry1.value {
+                assert_eq!(polygon.len(), 1);
+                assert_eq!(polygon[0].len(), 23);
+            } else {
+                panic!("Expected a Polygon geometry for feature1");
+            }
+            assert_eq!(
+                feature1
+                    .properties
+                    .as_ref()
+                    .unwrap()
+                    .get("filename")
+                    .unwrap(),
+                "tests/data\\input1.las"
+            );
+
+            // Check the second feature
+            let feature2 = &fc.features[1];
+            assert!(feature2.geometry.is_some());
+            let geometry2 = feature2.geometry.as_ref().unwrap();
+            if let geojson::Value::Polygon(polygon) = &geometry2.value {
+                assert_eq!(polygon.len(), 1);
+                assert_eq!(polygon[0].len(), 30); // Adjust the number of points as needed
+            } else {
+                panic!("Expected a Polygon geometry for feature2");
+            }
+            assert_eq!(
+                feature2
+                    .properties
+                    .as_ref()
+                    .unwrap()
+                    .get("filename")
+                    .unwrap(),
+                "tests/data\\input2.las"
+            );
+        } else {
+            panic!("Expected a FeatureCollection");
+        }
+
+        // Cleanup: Remove the output file
+        fs::remove_file(output_path).expect("Failed to delete the output file");
+    }
+
+    #[test]
+    fn test_integration_workflow_group_by_folder() {
+        let folder_path = "tests/data";
+        let result = process_folder(folder_path, false, true);
+        assert!(result.is_ok());
+
+        // Check if the output file is created
+        let output_path = Path::new("las_outlines.geojson");
+        assert!(output_path.exists());
+
+        // Read the file and perform checks
+        let geojson_str = fs::read_to_string(output_path).unwrap();
+        let geojson: GeoJson = geojson_str.parse().unwrap();
+        if let GeoJson::FeatureCollection(fc) = geojson {
+            assert!(!fc.features.is_empty());
+
+            // Check the number of features
+            assert_eq!(fc.features.len(), 1);
+
+            // Check the properties of the first feature
+            let feature = &fc.features[0];
+            assert!(feature.properties.is_some());
+            let properties = feature.properties.as_ref().unwrap();
+            assert_eq!(properties.get("folder_path").unwrap(), "tests/data");
+
+            // Check the geometry of the first feature
+            assert!(feature.geometry.is_some());
+            let geometry = feature.geometry.as_ref().unwrap();
+            if let Value::Polygon(coords) = &geometry.value {
+                // Check the number of coordinate sets (should be 1 for a simple polygon)
+                assert_eq!(coords.len(), 1);
+
+                // Check the number of coordinates in the exterior ring
+                let exterior_ring = &coords[0];
+                assert_eq!(exterior_ring.len(), 25);
+
+                // Check specific coordinates (e.g., the first and last)
+                assert_eq!(exterior_ring[0], vec![1771069.242, 5917200.036]);
+                assert_eq!(exterior_ring[24], vec![1771069.242, 5917200.036]);
+            } else {
+                panic!("Expected Polygon geometry");
+            }
+        } else {
+            panic!("Expected FeatureCollection");
+        }
+
+        // Clean up
+        fs::remove_file(output_path).unwrap();
+    }
 }
